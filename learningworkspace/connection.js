2b0fa516bde1ab93902134c3e633c680497ed510
NitinSharma1991 <nitin.rebelee91@gmail.com>
2020-04-16 18:15:16 +0530

    node

--- learningworkspace/connection.js
+++ learningworkspace/connection.js
@@ -1,19 +1,25 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 var sqlconnection = mysql.createConnection({
-    server: "localhost",
-    user: "dev",
-    password: "Mother08#1",
-    database: "Talentcentral",
-    multipleStatements: true,
-  });
-  
-  sqlconnection.connect((err) => {
-    if (!err) {
-      console.log("Connection Created");
-    } else {
-      console.log(err);
-    }
-  });
+  host: "localhost",
+  user: "dev",
+  password: "Mother08#1",
+  database: "Talentcentral",
+  port: 1433,
+  multipleStatements: true,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  }
+});
 
-  module.exports= sqlconnection;
+sqlconnection.connect((err) => {
+  if (!err) {
+    console.log("Connection Created");
+  } else {
+    console.log(err);
+  }
+});
+
+module.exports = sqlconnection;
--- learningworkspace/people.js
+++ learningworkspace/people.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const sqlconnection = require("./connection");
 router.get("/", (req, res) => {
     console.log("Query");
-  sqlconnection.query("select * from alerts_details", (err, res) => {
+  sqlconnection.query("select top 1 * from alerts_details", (err, rows) => {
     if (!err) {
-      res.send(res);
+      res.send(rows);
     } else {
       console.log(err);
       sqlconnection.destroy();
